7726f161e286f847b722e78bf4a1c7b427ecde89
coderofphcity <agantemboy@gmail.com>
2024-02-16 15:43:37 +0100

    working on test

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -5,4 +5,9 @@ interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,4 +5,6 @@ const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
 
+import '@openzeppelin/hardhat-upgrades';
+
 export default config;
